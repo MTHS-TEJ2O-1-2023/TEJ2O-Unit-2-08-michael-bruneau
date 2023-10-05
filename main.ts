1a36ca6283835dd879683ecdc6fdaae16aa00b44
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-05 12:32:57 -0400

    Setup

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael B
+ * Created on: Oct 2023
+ * This program shows all the coulours of RGB LED on micro bit
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+pins.digitalWritePin(DigitalPin.P13,0)
+pins.digitalWritePin(DigitalPin.P14,0)
+pins.digitalWritePin(DigitalPin.P15,0)
