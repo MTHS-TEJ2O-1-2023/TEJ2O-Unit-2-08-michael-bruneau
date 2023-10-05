fb0bf81f2978e2ddd38a4c29005daaf242f6392b
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-05 13:09:45 -0400

    Red and Blue

--- main.ts
+++ main.ts
@@ -13,7 +13,12 @@ pins.digitalWritePin(DigitalPin.P14, 0)
 pins.digitalWritePin(DigitalPin.P15, 0)
 
 input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
   pins.digitalWritePin(DigitalPin.P13, 1)
   basic.showString('Red')
+  basic.pause(1000)
+  
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  basic.showString('Blue')
   basic.pause(1000)
 })
